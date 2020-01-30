681d348832795e871f461a63e85157d6cfcfe256
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-30 18:47:44 +0100

    Neil's personal preferance style of redux function

--- src/features/testarea/testreducer.js
+++ src/features/testarea/testreducer.js
@@ -1,19 +1,31 @@
 import { INCREMENT_COUNTER, DECREMENT_COUNTER } from './testConstants';
+import { createReducer } from '../../app/common/util/reducerUtils';
 
 const initialState = {
   data: 42
 };
 
-const testReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case INCREMENT_COUNTER:
-      return { ...state, data: state.data + 1 };
-    case DECREMENT_COUNTER:
-      return { ...state, data: state.data - 1 };
-
-    default:
-      return state;
-  }
+const increment = state => {
+  return { ...state, data: state.data + 1 };
 };
 
-export default testReducer;
+const decrement = state => {
+  return { ...state, data: state.data - 1 };
+};
+
+export default createReducer(initialState, {
+  [INCREMENT_COUNTER]: increment,
+  [DECREMENT_COUNTER]: decrement
+});
+
+// const testReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case INCREMENT_COUNTER:
+//       return { ...state, data: state.data + 1 };
+//     case DECREMENT_COUNTER:
+//       return { ...state, data: state.data - 1 };
+
+//     default:
+//       return state;
+//   }
+// };
