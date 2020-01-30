6d2382cd89a398b6e9180b57ac6349e7e77318f4
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-30 18:20:41 +0100

    add devToolsEnhancer

--- src/app/store/configeStore.js
+++ src/app/store/configeStore.js
@@ -1,8 +1,9 @@
 import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import rootReducer from '../../app/reducers/rootreducer';
 
 export const configueStore = () => {
-  const store = createStore(rootReducer);
+  const store = createStore(rootReducer, devToolsEnhancer());
   return store;
 };
 
